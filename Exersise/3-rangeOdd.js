d664f916c6c1fafd530efbae35f9026178d2ecf1
TORESTO <therealtoresto@gmail.com>
2021-03-02 12:17:24 +0200

    added file

---
+++ Exersise/3-rangeOdd.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const rangeOdd = (start, end) => {
+    const mas = [];
+    let index = 0;
+    for (let i = start; i <= end; i++ ) {
+        if (i % 2 !== 0) {
+            mas[index] = i;
+            index++;
+        }
+    }
+    return mas;
+}
+
+console.log(rangeOdd(2 ,9));
