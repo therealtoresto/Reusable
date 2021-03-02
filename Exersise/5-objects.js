e15e1adad0d4dca37e8ae0f60e8f767b7dc28ae1
TORESTO <therealtoresto@gmail.com>
2021-03-02 18:43:23 +0200

    added file

---
+++ Exersise/5-objects.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fn = () => {
+  const obj1 = { name: 'Marcus' };
+  let obj2 = { name: 'Marcus' };
+
+  obj1.name = 'Aurelius';
+  obj2.name = 'Aurelius';
+
+  obj2 = { name: 'Marcus Aurelius' };
+};
+
+const createUser = (name, city) => ({
+    name: `${name}`,
+    city: `${city}`  
+});
+
+console.dir(createUser('Marcus', 'Roma'));
